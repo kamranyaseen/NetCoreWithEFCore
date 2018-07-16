3c7b428857818eaa20d588b4a62347695a0efb0e
Kamran Yaseen <kamranmyaseen@gmail.com>
2018-07-16 14:46:08 +0300

    .Net Core & EF Core with Knockout & Swagger
    This web application uses modern UI and RESTful API, and as long as interfaces are the same, could be easily swapped out with a different application. RESTful API written in loosely coupled manner to keep the maintenance easier, and unit tests included to cover the logic implemented. The application requires minimal configuration to start.

---
+++ WebApplication1.Web/wwwroot/app/services/http.service.js
@@ -0,0 +1,43 @@
+﻿define(["jquery"], function (jquery) {
+
+    function HttpService() {
+        return {
+            get: getMethod,
+            post: postMethod,
+            put: putMethod,
+            delete: deleteMethod
+        };
+    }
+
+    function getMethod(url) {
+        return ajaxCall(url, "GET");
+    };
+
+    function postMethod(url, data) {
+        return ajaxCall(url, "POST", data);
+    };
+
+    function putMethod(url, data) {
+        return ajaxCall(url, "PUT", data);
+    };
+
+    function deleteMethod(url) {
+        return ajaxCall(url, "DELETE");
+    };
+
+    function ajaxCall(url, method, data) {
+        return jquery.ajax({
+            url: url,
+            method: method,
+            data: JSON.stringify(data),
+            headers: {
+                'Accept': "application/json",
+                'Content-Type': "application/json; charset=utf-8"
+            },
+            dataType: "json",
+            jsonp: false
+        });
+    }
+
+    return HttpService();
+});
